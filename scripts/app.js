6e650d1bba248094de6d2380106ebd33bb57fbae
Ospon <ospania@gmail.com>
2020-01-26 21:41:23 +0100

    Added animations and fixed bugs

---
+++ scripts/app.js
@@ -0,0 +1,52 @@
+jQuery(document).ready(function() {
+    
+    jQuery('.mainOffer').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.advantages').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.whyUs').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.numbers').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.about').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.gallery').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.team__titleGroup').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.team__item').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.consultation').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+
+    jQuery('.footer').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animated fadeIn',
+        offset: 300
+    });
+});
